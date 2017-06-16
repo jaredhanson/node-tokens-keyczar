41589f603ce94cad60742b280009822623e69cb6
Jared Hanson <jaredhanson@gmail.com>
2017-06-16 13:02:16 -0700

    WIP RSA implementation.

--- test/seal.test.js
+++ test/seal.test.js
@@ -1,6 +1,7 @@
 var keyczar = require('keyczarjs');
 var keyczar_util = require('keyczarjs/keyczar_util');
 var setup = require('../lib/seal');
+var fs = require('fs');
 var sinon = require('sinon');
 var expect = require('chai').expect;
 
@@ -18,6 +19,11 @@ describe('seal', function() {
           } else {
             return cb(null, [ { secret: 'abcdef7890abcdefef7890abcdef7890' } ]);
           }
+        }
+        
+        switch (q.recipient.id) {
+        case 'https://api.example.com/rsa-2048':
+          return cb(null, [ { publicKey: fs.readFileSync(__dirname + '/keys/rsa-2048/cert.pem'), algorithm: 'rsa-oaep' } ]);
         }
       });
       
@@ -100,6 +106,86 @@ describe('seal', function() {
         });
       });
     }); // encrypting to self
+    
+    describe.skip('encrypting to audience with 2048-bit RSA', function() {
+      this.timeout(10000);
+      
+      var token;
+      before(function(done) {
+        var audience = [ {
+          id: 'https://api.example.com/rsa-2048'
+        } ];
+        
+        seal({ foo: 'bar' }, { audience: audience }, function(err, t) {
+          token = t;
+          done(err);
+        });
+      });
+      
+      after(function() {
+        keying.reset();
+      });
+      
+      it('should query for key', function() {
+        expect(keying.callCount).to.equal(2);
+        var call = keying.getCall(0);
+        expect(call.args[0]).to.deep.equal({
+          usage: 'encrypt',
+          recipient: undefined,
+          algorithms: [ 'aes128-cbc' ]
+        });
+        
+        call = keying.getCall(1);
+        expect(call.args[0]).to.deep.equal({
+          usage: 'sign',
+          recipient: undefined,
+          algorithms: [ 'hmac-sha256' ]
+        });
+      });
+      
+      it('should generate a token', function() {
+        expect(token.length).to.be.above(0);
+        expect(token.substr(0, 1)).to.equal('A');
+      });
+      
+      describe('verifying claims', function() {
+        var claims;
+        before(function() {
+          var szkeyset = JSON.stringify({
+            meta: JSON.stringify({
+              name: '',
+              purpose: keyczar.PURPOSE_DECRYPT_ENCRYPT,
+              type: keyczar.TYPE_AES,
+              encrypted: false,
+              versions: [{
+                exportable: false,
+                status: 'PRIMARY',
+                versionNumber: 1
+              }]
+            }),
+            '1': JSON.stringify({
+              mode: 'CBC',
+              aesKeyString: keyczar_util.encodeBase64Url('abcdef7890abcdef'),
+              size: 128,
+              hmacKey: {
+                hmacKeyString: keyczar_util.encodeBase64Url('abcdef7890abcdefef7890abcdef7890'),
+                size: 256
+              }
+            })
+          });
+          
+          var keyset = keyczar.fromJson(szkeyset);
+          var payload = keyset.decrypt(token);
+          
+          claims = JSON.parse(payload);
+        });
+        
+        it('should be correct', function() {
+          expect(claims).to.be.an('object');
+          expect(claims.foo).to.equal('bar');
+        });
+      });
+    }); // encrypting to audience with 2048-bit RSA
     
   });
   
